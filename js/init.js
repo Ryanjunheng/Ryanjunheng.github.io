5108e80e19fa5925a03ddfc2db54f94fc043915c
Ryan <junheng102@gmail.com>
2018-02-18 17:25:03 +0800

    Fixed js

--- js/init.js
+++ js/init.js
@@ -4,12 +4,14 @@
 /*
 -----------------------------------------------------------------------------------*/
 
- jQuery(document).ready(function($) {
-
-  $(window).load(function() {
+$(window).load(function() {
     // Animate loader off screen
     $(".se-pre-con").fadeOut("slow");;
   });
+
+ jQuery(document).ready(function($) {
+
+  
 
 /*----------------------------------------------------*/
 /* FitText Settings
